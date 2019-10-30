94a9d1247c6abfa32b78e65fd546fbfb12dd12e3
EC2 Default User <ec2-user@ip-172-31-18-153.us-east-2.compute.internal>
2019-10-30 22:29:15 +0000

    Added dynamic manage users table

--- static/js/manage_users.js
+++ static/js/manage_users.js
@@ -1,6 +1,24 @@
 /* 
   Add databtales to display user data or w.e
 */
+var dataSet = [
+    [ "Tiger_Nixon", "Professor", "3", "tiger_nixon@tamu.edu", "610-432-2121", "Star, Spec","01/13/2019(21:45 GMT)"],
+    [ "Garrett_Winters", "Research Assistant", "1", "garrett_winters@tamu.edu", "230-845-5846", "Web, Hrbb","10/24/2019(5:30 GMT)"],
+    [ "James_Brown", "Research Assistant", "1", "j_brown@tamu.edu", "430-765-3446", "Spy, Nxt","08/01/2019(8:32 GMT)"]
+];
+
 $(document).ready(function() {
- 
+    $('#users_table').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "User" },
+            { title: "Position" },
+            { title: "Access" },
+            { title: "Email" },
+            { title: "Phone Number" },
+            { title: "Groups" },
+            { title: "Last Login" },
+            
+        ]
+    } );
 });
