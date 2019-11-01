c7c92571879d85be6722d284f83255d4a141b4c9
Ubuntu <ubuntu@ip-172-31-11-212.us-east-2.compute.internal>
2019-11-01 18:12:58 +0000

    Initial message code

--- model/user_manager.py
+++ model/user_manager.py
@@ -28,8 +28,14 @@ def get_user_profile(user):
   
 def get_all_users():
   # TODO
-  # Return a list of users and their phone/email
-  return True
+  
+  # Replace data[] with query of all users
+  data = [
+    [ "Tiger_Nixon", "Professor", "3", "tiger_nixon@tamu.edu", "610-432-2121", "Star, Spec","01/13/2019(21:45 GMT)"],
+    [ "Garrett_Winters", "Research Assistant", "1", "garrett_winters@tamu.edu", "230-845-5846", "Web, Hrbb","10/24/2019(5:30 GMT)"],
+    [ "James_Brown", "Research Assistant", "1", "j_brown@tamu.edu", "430-765-3446", "Spy, Nxt","08/01/2019(8:32 GMT)"]
+  ]
+  return data
   
 def delete_user():
   # TODO
