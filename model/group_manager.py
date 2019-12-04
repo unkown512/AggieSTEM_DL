c39bd830ac0bacd40a2af2d066aad3c73deadc7b
SithTech <ottsel62@gmail.com>
2019-12-04 16:24:52 -0600

    Fixed a few things
    -Migrated the system over to the new ID system
    -Fixed various bugs in all model scripts
    -Removed random number at the end of usernames. This is in prep for the email based login

--- model/group_manager.py
+++ model/group_manager.py
@@ -1,9 +1,9 @@
 # Model for user group interaction
-import mongoengine
-import pymongo
-import datetime
-import json
-import sys
+#import mongoengine
+#import pymongo
+#import datetime
+#import json
+#import sys
 
 # Checks if the access level being set is appropriate for the DB
 # 0 : all access, 1 : tier 1 access, 2 : tier 2 access, ..., 5 : admin access.
@@ -16,27 +16,32 @@ def valid_access_level(access_level):
     return False
 
 # Create a new group
-def create_group(db, user_id, user_list, access_level):
+def create_group(db, owner_id, user_ids, access_level, group_name=None):
   # Check if the access_level is valid.
   if not valid_access_level(access_level):
     print("Invalid access level requested!")
     return False
 
   # Check if a member is being included with this group.
-  if len(user_list) == 0:
+  if user_ids is None:
     print("Critical error! Must have at least one member!")
     return False
 
+  # Check for a group_name, otherwise set it to the default
+  if group_name is None:
+    group_name = 'New Group'
+
   # Next, build the group JSON dictionary.
-  _id = db['group'].insert_one({
-    'owner_id'     : user_id,
-    'user_list'    : user_list,
+  new_group_id = db['group'].insert_one({
+    'name'         : group_name,
+    'owner_id'     : owner_id,
     'access_level' : access_level,
-    'user_ids'     : [user_id]
+    'user_ids'     : user_ids,
+    'deleted'      : False
   })
 
   # Check if this insert was successful
-  if is_group(db, _id):
+  if is_group(db, str(new_group_id.inserted_id)):
     print("Successful insertion of group!")
     return True
   else:
@@ -66,7 +71,11 @@ def delete_group(db, user_id, group_id):
 
   # At this point, all data retrieved from the db has been verified, and the
   # delete transaction can take place.
-  db['group'].delete_one({"_id" : group_id})
+  #db['group'].delete_one({"_id" : group_id})
+
+  # Don't actually delete the group, but set the deleted flag for the group
+  group['deleted'] = True
+  db['group'].update_one({"_id" : group_id}, {'$set': group})
 
   # Check if the group was successfully deleted.
   if not is_group(db, group_id):
@@ -85,12 +94,12 @@ def get_users(db, group_id):
     return False
 
   # Query the groups array for values equivalent to group_id.
-  group    = db['group'].find({"_id" : group_id})
-  users    = db['user'].find({"_id"  : group['user_ids']})
+  group    = db['group'].find({'_id' : group_id})
+  users    = db['user'].find({'_id'  : group['user_ids']})
 
   # Check if the group has users to return.
   # Base user should be the admin - this is a critical error if hit.
-  if len(users) == 0:
+  if users is None:
     print("Critical error! No users in this group!")
     return False
 
@@ -111,10 +120,15 @@ def get_users(db, group_id):
 # Valid group check.
 # https://stackoverflow.com/questions/25163658/mongodb-return-true-if-document-exists
 def is_group(db, group_id):
-  # Query to see if a single library with this id exists.
+  # Query to see if a single group with this id exists and the deleted flag has not been set.
   if db['group'].count_documents({"_id" : group_id}, limit = 1) != 0:
-    print("Valid group!")
-    return True
+    is_deleted = db['group'].find_one({"_id" : group_id})
+    if is_deleted['deleted'] == False:
+      print("Valid group!")
+      return True
+    else:
+      print('Group has been deleted!')
+      return False
   # If the query returned 0, the group/library doesn't exist.
   else:
     print("Invalid group!")
@@ -129,7 +143,7 @@ def add_user(db, admin_id, group_id, user_id):
 
   # Check if the user being entered exists
   user = db['user'].find_one({"_id" : user_id})
-  if len(user) == 0:
+  if user is None:
     print("Invalid user_id! Cannot add to group!")
     return False
 
@@ -137,8 +151,8 @@ def add_user(db, admin_id, group_id, user_id):
   admin = db['user'].find_one({"_id"  : admin_id})
   group = db['group'].find_one({"_id": group_id})
 
-  # Check if the admin has correct credentials.
-  if len(admin) == 0 or admin['access_level'] < group['access_level']:
+  # Check if the admin has correct credentials or is the owner.
+  if admin is None or admin['access_level'] < group['access_level'] or str(admin['_id']) != group['owner_id']:
     print("Invalid credentials to add user to group!")
     return False
   # If so, add this user to the group.
@@ -157,7 +171,7 @@ def remove_user(db, admin_id, group_id, user_id):
 
   # Check if the user being entered exists
   user = db['user'].find_one({"_id" : user_id})
-  if len(user) == 0:
+  if user is None:
     print("Invalid user_id! Cannot add to group!")
     return False
 
@@ -165,15 +179,15 @@ def remove_user(db, admin_id, group_id, user_id):
   admin = db['user'].find_one({"_id"  : admin_id})
   group = db['group'].find_one({"_id": group_id})
 
-  # Check if the admin has correct credentials.
-  if len(admin) != 0 or admin['access_level'] >= group['access_level']:
+  # Check if the admin has correct credentials or is the owner.
+  if admin is None or admin['access_level'] >= group['access_level'] or str(admin['_id']) != group['owner_id']:
     print("Valid credentials! Adding...")
     group['user_ids'].remove(user_id)
     db['group'].update(group)
     return True
   # Otherwise, do not delete.
   else:
-    print("Invalid credentials to add user to group!")
+    print("Invalid credentials to remove user from group!")
     return False
 
 # Checks if the user has the appropriate permissions, and if so, adds the group to their document.
@@ -185,7 +199,7 @@ def join_group(db, user_id, group_id):
 
   # Check if the user being entered exists
   user = db['user'].find_one({"_id" : user_id})
-  if len(user) == 0:
+  if user is None:
     print("Invalid user_id! Cannot add to group!")
     return False
 
@@ -205,16 +219,27 @@ def join_group(db, user_id, group_id):
 # Grabs all groups that this user is a part of.
 def get_all_groups(db, user_id):
   # Grab this user to check availability
-  user = db['user'].find_one({"_id": user_id})
-  if len(user) == 0:
-      print("Error! Invalid user_id given! Cannot retrieve groups!")
-      return False
+  user = db['user'].find_one({'_id': str(user_id)})
+  print(user)
+  if user is None:
+    print("Error! Invalid user_id given! Cannot retrieve groups!")
+    return False
   
+  user_groups = db['group'].find({'user_ids': user_id})
+
+  if user_groups is None:
+    print('User is not in a group!')
+    return False
+  return user_groups
+
+
+  '''
   # Grab the groups and check for correctness.
-  user_access = db['user_library_access'].find_one({"_id": user_id})
+  user_access = db['user_library_access'].find_one({"user_id": user_id})
   if len(user_access) == 0:
       print("Error! Could not retrieve user_library_access!")
       return False
   
   #Return all the groups the user is apart of
   return user_access["library_ids"]
+  '''
--- model/library_manager.py
+++ model/library_manager.py
@@ -2,14 +2,14 @@
    Model team to fill in interations with library
 
 '''
-
+# TODO: Change 'library_id' to 'library_name'
 def create_library(db, owner_id, library_id, min_perm, content_ids):
   '''
     library.owner = userid of who can access this
     library.content_id_list = list of conten_ids
   '''
   # Check if the intended owner exists and has the appropriate access level
-  owner = db["user"].find({"user_id": owner_id})
+  owner = db["user"].find({"_id": owner_id})
   if len(owner) == 0:
       print("Error! Intended owner account does not exist!")
       return False
@@ -18,13 +18,23 @@ def create_library(db, owner_id, library_id, min_perm, content_ids):
       return False
   
   # Next, build the library JSON dictionary.
-  db['library'].insert_one({
-    'library_id'    : library_id,
+  new_library_id = db['library'].insert_one({
+    'name'          : library_id,
     'owner_id'      : owner_id,
     'min_permission': min_perm,
-    'content_ids'   : [content_ids]
+    'content_ids'   : [content_ids],
+    'deleted'       : False
   })
 
+  # Update user's access table to include the new library
+  user_access = db['access'].find_one({'user_id': owner_id})
+  user_access['library_ids'] = list(user_access['library_ids'].append(str(new_library_id.inserted_id)))
+
+  # It is assumed that the owner should have admin rights over their library
+  user_access['library_access'] = list(user_access['library_access'].append(3))
+  #user_access['library_access'] = set(user_access['library_access'].append(owner["access_level"]))
+
+  # TODO: Add test for if it was created successfully and the owner has access
   # Successful operation.
   return True
 
@@ -34,7 +44,7 @@ def add_content(db, user_id, library_id, content_name, data):
     content.dir = path to data
   '''
   # Get the library to add the content id to.
-  library = db["library"].find_one({"library_id": library_id})
+  library = db["library"].find_one({"_id": library_id})
   if len(library) == 0:
       print("Error! Adding to a nonextant library...")
       return False
@@ -43,26 +53,37 @@ def add_content(db, user_id, library_id, content_name, data):
       return False
   
   # Next, build the group JSON dictionary.
-  _id = db['content'].insert_one({
+  new_content_id = db['content'].insert_one({
+    'name'         : content_name,
     'data'         : data,
-    'content_id'   : content_name
+    'deleted'      : False
   })
     
   # Add the inserted record's ID to the library
-  library["content_ids"] = set(library["content_ids"].append(_id))
+  library["content_ids"] = list(library["content_ids"].append(str(new_content_id.inserted_id)))
     
   # Update the library w/ the new content id
-  db['library'].update_one({'library_id': library_id}, {'$set': library})
+  db['library'].update_one({'_id': library_id}, {'$set': library})
   
+  # TODO: Add test for if it was successful
   # Successful
   return True
 
 # TODO: Add a delete flag for content 
-def delete_content(db, content):
+def delete_content(db, content_id):
   '''
     content.id = content_id
   '''
-  return True
+  content_to_delete = db['content'].find_one({'_id': content_id})
+  content_to_delete['deleted'] = True
+  db['content'].update_one({'_id': content_id}, {'$set': content_to_delete})
+
+  # Test for content deletion
+  is_deleted = db['content'].find_one({'_id': content_id})
+  if is_deleted['deleted']:
+    return True
+  else:
+    return False
 
 # TODO: Add a delete flag for libraries
 def delete_library(db, library):
